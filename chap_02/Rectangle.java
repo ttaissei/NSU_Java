dda89dd6dcab628817f2a1c78fc3e881208ea06a
DONGJU CHOI <162415042+ttaissei@users.noreply.github.com>
2024-03-13 16:43:01 +0900

    Add files via upload

---
+++ chap_02/Rectangle.java
@@ -0,0 +1,20 @@
+package chap_02;
+
+import java.util.Scanner;
+
+public class Rectangle {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		double w, h, area;
+		Scanner in = new Scanner(System.in);
+		System.out.print("직사각형의 가로 길이 : ");
+		w = in.nextInt();
+		
+		System.out.print("직사각형의 세로 길이 : ");
+		h = in.nextInt();
+		
+		
+	}
+
+}
