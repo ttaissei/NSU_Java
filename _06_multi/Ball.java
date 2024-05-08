b88e2c8b92f25938e83e50e7a154eb4cab384712
DONGJU CHOI <162415042+ttaissei@users.noreply.github.com>
2024-05-08 14:38:50 +0900

    Add files via upload

---
+++ _06_multi/Ball.java
@@ -0,0 +1,15 @@
+package _06_multi;
+
+public class Ball extends Circle{
+    private String color;
+    public Ball(String color) {
+        this.color = color;
+    }
+
+    public void findColor() {
+        System.out.println(color + " 공이다.");
+    }
+    public void findVolume() {
+        System.out.println("부피는 4/3 * (π * 반지름 * 반지름 * 반지름)이다.");
+    }
+}
---
+++ _06_multi/Circle.java
@@ -0,0 +1,15 @@
+package _06_multi;
+
+public class Circle {
+    private void secret() {
+        System.out.println("비밀이다.");
+    }
+
+    protected void findRadius() {
+        System.out.println("반지름이 10.0센티이다.");
+    }
+
+    public void findArea() {
+        System.out.println("보이는 (π * 반지름 * 반지름)이다.");
+    }
+}
